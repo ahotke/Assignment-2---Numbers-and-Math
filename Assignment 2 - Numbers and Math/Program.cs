722e705a1ac74c85727220d0df6be1432c421ab7
Ashton Hotke <339741886@GoTVDSB.ca>
2024-09-06 09:27:14 -0400

    Add project files.

---
+++ Assignment 2 - Numbers and Math/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment_2___Numbers_and_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This was made by Ashton
+
+            Console.WriteLine("I was born in" + (2024 - 16));
+            Console.WriteLine("");
+            Console.WriteLine("The average of 14, 15 and 16 is " + ((14 + 15 + 16) / 3.0));
+            Console.WriteLine("");
+            Console.WriteLine("The average of 14, 15, 16 and 17 is " + ((14 + 15 + 16 + 17) / 4.0));
+            Console.WriteLine("");
+            Console.Write("The area of a circle with a radius of 3cm is ");
+            Console.WriteLine((3 * 3) * Math.PI + " cm");
+            Console.WriteLine("");
+            Console.Write("C# uses BEDMAS to determine whether it needs to add or concactinate");
+            Console.WriteLine("");
+        }
+    }
+}
